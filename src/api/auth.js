af094bcb30732872b65231543e51e2c2e7e473dd
jasonjohn12 <jasonjohn12@gmail.com>
2022-02-12 13:03:24 -0600

    remove comment

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,7 +5,6 @@ const api = axios.create({
   timeout: 1000,
   headers: {
     Accept: "application/json",
-    //'Authorization': 'token <your-token-here> -- https://docs.GitHub.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token'
   },
 });
 
